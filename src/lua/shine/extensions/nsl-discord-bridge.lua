8bb0674b83e84f1b5b3f5323f9a349ed806b9ac3
adsfgg <11364579+adsfgg@users.noreply.github.com>
2024-01-28 18:36:58 +0000

    Switch to shine properly

--- src/lua/shine/extensions/nsl-discord-bridge.lua
+++ src/lua/shine/extensions/nsl-discord-bridge.lua
@@ -14,44 +14,32 @@ Plugin.CheckConfigRecursively = false
 Plugin.DefaultState = true
 
 function Plugin:Initialise()
-    local filepath = "config://NSL-Discord-Bridge.json"
-    local configFile = io.open(filepath, "r")
-    if configFile then
-        local discord_bridge_config = json.decode(configFile:read("*all"))
-        Plugin.Config.discordWebhookUrl = discord_bridge_config['discordWebhookUrl']
-        self:SaveConfig()
-        io.close(configFile)
-
-        os.remove(filepath)
+    if self.Config.discordWebhookUrl == "" then
+        local filepath = "config://NSL-Discord-Bridge.json"
+        local configFile = io.open(filepath, "r")
+        if configFile then
+            local discord_bridge_config = json.decode(configFile:read("*all"))
+            Plugin.Config.discordWebhookUrl = discord_bridge_config['discordWebhookUrl']
+            self:SaveConfig()
+            io.close(configFile)
+        end
     end
 
     return true
 end
 
-local function PostMessage(msg)
-    assert(msg)
-    assert(string.len(msg) > 0)
-
+function Plugin:EndGame( Gamerules, WinningTeam )
+    Print( "Round ended!!" )
+    local teamName = WinningTeam and Shine:GetTeamName(WinningTeam:GetTeamNumber(), true) or "Draw"
+    Print( teamName )
     params = {
-        content=string.format("%s|%s|%s", msg, Server.GetIpAddress(), Server.GetPort()),
+        content=string.format("%s|%s|%s", teamName, Server.GetIpAddress(), Server.GetPort()),
         username="Match Reporter"
     }
-    Shared.SendHTTPRequest(kURL, "POST", params,
-        function(data)
-            msg = string.UTF8Sub("Round data sent to Scrim bot", 1, kMaxChatLength)
-            Server.SendNetworkMessage("Chat", BuildChatMessage(false, "Discord Bridge",-1, kTeamReadyRoom, kNeutralTeamType, "Round recorded in Discord bot"), true)
-        end
-    )
-end
-
-function Plugin:EndGame( Gamerules, WinningTeam )
-    if WinningTeam == kGameState.Team1Won then
-        PostMessage("Marines won")
-    elseif WinningTeam == kGameState.Team2Won then
-        PostMessage("Aliens won")
-    elseif WinningTeam == kGameState.Draw then
-        PostMessage("Draw???")
-    end
+    Shared.SendHTTPRequest(self.Config.discordWebhookUrl, "POST", params, function(data)
+        teamName = string.UTF8Sub("Round data sent to Scrim bot", 1, kMaxChatLength)
+        Server.SendNetworkMessage("Chat", BuildChatMessage(false, "Discord Bridge",-1, kTeamReadyRoom, kNeutralTeamType, "Round recorded in Discord bot"), true)
+    end)
 end
 
 return Plugin
